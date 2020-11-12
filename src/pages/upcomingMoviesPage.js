a1e14b529a2adb04c14c99757b66b0137db6bb97
Kate Furlong <furlong.kd@gmail.com>
2020-11-12 09:08:59 +0000

    Fix upcoming movies page

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import StubAPI from "../api/stubAPI";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavoritesButton from "../components/buttons/addToFavorites";
 
 const UpcomingMovieListPage = () => {
   const [movies, setMovies] = useState([]);
@@ -11,21 +12,23 @@ const UpcomingMovieListPage = () => {
     });
   }, []);
 
-  const addToFavorites = (movieId) => {
-    setMovies((movies) => {
-      const index = movies.map((m) => m.id).indexOf(movieId);
-      StubAPI.add(movies[index]);
-      let newMoviesState = [...movies];
-      newMoviesState.splice(index, 1);
-      return newMoviesState;
-    });
-  };
+  // const addToFavorites = (movieId) => {
+  //   setMovies((movies) => {
+  //     const index = movies.map((m) => m.id).indexOf(movieId);
+  //     StubAPI.add(movies[index]);
+  //     let newMoviesState = [...movies];
+  //     newMoviesState.splice(index, 1);
+  //     return newMoviesState;
+  //   });
+  // };
 
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      buttonHandler={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesButton movie={movie} />;
+      }}
     />
   );
 };
