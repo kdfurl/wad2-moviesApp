570284e6cfe4190726bb9e34717cfbcabda9a8b0
Kate Furlong <furlong.kd@gmail.com>
2020-11-12 09:27:48 +0000

    Change upcoming movies page to use context

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from "react";
-import StubAPI from "../api/stubAPI";
+import React, { useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import { getUpcomingMovies } from "../api/tmdb-api";
+import { MoviesContext } from "../contexts/moviesContext";
 import AddToWatchListButton from "../components/buttons/addToWatchList";
 
 const UpcomingMovieListPage = () => {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    getUpcomingMovies().then((movies) => {
-      setMovies(movies);
-    });
-  }, []);
-
-  // const addToFavorites = (movieId) => {
-  //   setMovies((movies) => {
-  //     const index = movies.map((m) => m.id).indexOf(movieId);
-  //     StubAPI.add(movies[index]);
-  //     let newMoviesState = [...movies];
-  //     newMoviesState.splice(index, 1);
-  //     return newMoviesState;
-  //   });
-  // };
+  const context = useContext(MoviesContext);
+  const movies = context.upcoming;
 
   return (
     <PageTemplate
