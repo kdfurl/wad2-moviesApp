0149736d5765f7c597ea76ef9e6e30c68eb46da6
Kate Furlong <furlong.kd@gmail.com>
2020-11-16 04:42:46 +0000

    E2E tests - Navigation - Movies Details page Back button

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -78,4 +78,26 @@ describe("Navigation", () => {
       cy.get("h2").contains(movies[0].title);
     });
   });
+
+  describe("The Go Back button", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+    it("should navigate from home page to movie details and back", () => {
+      cy.get(".card").eq(1).find("img").click();
+      cy.get("svg[data-icon=arrow-circle-left]").click();
+      cy.url().should("not.include", `/movies`);
+      cy.get("h2").contains("No. Movies");
+    });
+    it("should navigate from favorites page to movie details and back", () => {
+      cy.get(".card").eq(0).find("button").click();
+      cy.get("nav").find("li").eq(2).find("a").click();
+      cy.url().should("include", `/favorites`);
+      cy.get(".card").eq(0).find("img").click();
+      cy.url().should("include", `/movies/${movies[0].id}`);
+      cy.get("svg[data-icon=arrow-circle-left]").click();
+      cy.url().should("include", `/favorites`);
+      cy.get("h2").contains("Favorite Movies");
+    });
+  });
 });
